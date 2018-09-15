63d7dd5d5215a0f03c5b7361ad028bd96dd76f1d
artmaro <artmaro@gmail.com>
2018-09-15 13:49:44 +0300

    talks page

--- webpack.config.js
+++ webpack.config.js
@@ -111,6 +111,7 @@ module.exports = env => {
       generateHtmlFile('src/html/privacy-template.html', 'privacy.html'),
       generateHtmlFile('src/html/cfp-template.html', 'cfp.html'),
       generateHtmlFile('src/html/training-ml-template.html', 'training-ml.html'),
+      generateHtmlFile('src/html/talks-template.html', 'talks.html'),
       generateHtmlFile('src/html/yappidays2017-template.html', 'yappidays2017.html'),
       generateHtmlFile('src/html/yappidays2017-talks/yappidays2017-hadoop.html', 'yappidays2017-hadoop.html'),
       generateHtmlFile('src/html/yappidays2017-talks/yappidays2017-ci.html', 'yappidays2017-ci.html'),
